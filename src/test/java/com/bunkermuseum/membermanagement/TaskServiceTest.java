5cc0fc2142f373904248650e4eb6ca188cc3574b
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-09-22 16:04:15 +0200

    Rename `taskmanagement` package to `membermanagement` across all module files for consistency with module restructuring.

--- src/test/java/com/bunkermuseum/membermanagement/TaskServiceTest.java
+++ src/test/java/com/bunkermuseum/membermanagement/TaskServiceTest.java
@@ -1,4 +1,4 @@
-package com.bunkermuseum.taskmanagement;
+package com.bunkermuseum.membermanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;

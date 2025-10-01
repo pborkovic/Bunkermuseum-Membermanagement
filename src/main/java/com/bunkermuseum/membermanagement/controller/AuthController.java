d1be12277facaebd9b4c7da4d212817fd1bae005
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-10-01 12:55:32 +0200

    Inject `UserServiceContract` and `RoleServiceContract` into `AuthController` constructor

--- src/main/java/com/bunkermuseum/membermanagement/controller/AuthController.java
+++ src/main/java/com/bunkermuseum/membermanagement/controller/AuthController.java
@@ -1,4 +1,17 @@
 package com.bunkermuseum.membermanagement.controller;
 
+import com.bunkermuseum.membermanagement.service.contract.RoleServiceContract;
+import com.bunkermuseum.membermanagement.service.contract.UserServiceContract;
+
 public class AuthController {
+    private final UserServiceContract userService;
+    private final RoleServiceContract roleService;
+
+    public AuthController(
+            UserServiceContract userService,
+            RoleServiceContract roleService
+    ) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
 }

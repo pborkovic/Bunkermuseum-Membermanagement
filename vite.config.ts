2c1917ad1fa4e6379672e910e4bf21902c5d11f6
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-10-03 14:24:29 +0200

    Add alias configuration for `@` to simplify imports in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { UserConfigFn } from 'vite';
 import { overrideVaadinConfig } from './vite.generated';
+import path from 'path';
 
 const customConfig: UserConfigFn = (env) => ({
   // Here you can add custom Vite parameters
   // https://vitejs.dev/config/
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/main/frontend'),
+    },
+  },
 });
 
 export default overrideVaadinConfig(customConfig);

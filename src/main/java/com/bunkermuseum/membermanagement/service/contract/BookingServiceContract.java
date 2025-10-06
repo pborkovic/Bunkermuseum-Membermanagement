fc453d70bbe40741d3f1fefd80a63db1a907ebd5
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-10-06 15:11:55 +0200

    Add `getAllBookings` method in `BookingServiceContract` to define contract for fetching all bookings

--- src/main/java/com/bunkermuseum/membermanagement/service/contract/BookingServiceContract.java
+++ src/main/java/com/bunkermuseum/membermanagement/service/contract/BookingServiceContract.java
@@ -10,8 +10,24 @@ import com.bunkermuseum.membermanagement.model.Booking;
  * for maximum flexibility in implementation while maintaining clear separation
  * of concerns.</p>
  *
+ * @author Philipp Borkovic
  * @see Booking
  * @see com.bunkermuseum.membermanagement.service.base.BaseService
  */
 public interface BookingServiceContract {
+
+    /**
+     * Retrieves all bookings from the system.
+     *
+     * <p>This method fetches all bookings from the database.
+     * It's intended for administrative purposes and should be protected
+     * by appropriate authorization checks.</p>
+     *
+     * @return List of all bookings in the system
+     *
+     * @throws RuntimeException if retrieval fails
+     *
+     * @author Philipp Borkovic
+     */
+    java.util.List<Booking> getAllBookings();
 }

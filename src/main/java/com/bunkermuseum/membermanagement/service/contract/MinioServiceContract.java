ce9aff2aba7456ed710db39ea13ffae3ef52be48
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-10-22 14:30:22 +0200

    Move `MinioServiceContract` to `service.contract` package for better organization.
    Signed-off-by: Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>

--- src/main/java/com/bunkermuseum/membermanagement/service/contract/MinioServiceContract.java
+++ src/main/java/com/bunkermuseum/membermanagement/service/contract/MinioServiceContract.java
@@ -1,5 +1,6 @@
-package com.bunkermuseum.membermanagement.service;
+package com.bunkermuseum.membermanagement.service.contract;
 
+import com.bunkermuseum.membermanagement.service.MinioService;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;

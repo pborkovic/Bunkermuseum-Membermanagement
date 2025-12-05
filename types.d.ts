86d1561f08b70a1272f6fc503dcd33091aa806a2
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-05 10:43:08 +0100

    Extend `ImportMeta` definitions to include `VITE_RECAPTCHA_SITE_KEY` for environment variable typing.
    Signed-off-by: Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>

--- types.d.ts
+++ types.d.ts
@@ -46,3 +46,11 @@ declare module '*.webp' {
   const content: string;
   export default content;
 }
+
+interface ImportMetaEnv {
+  readonly VITE_RECAPTCHA_SITE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

2bbe0faf131317a5ea6c7bdde4fe1895c24ca95a
Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>
2025-12-05 13:57:07 +0100

    Remove unnecessary blank line in `ReCaptchaService` imports.
    Signed-off-by: Philipp Borkovic <philipp.borkovic@edu.htl-villach.at>

--- src/main/java/com/bunkermuseum/membermanagement/service/ReCaptchaService.java
+++ src/main/java/com/bunkermuseum/membermanagement/service/ReCaptchaService.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
